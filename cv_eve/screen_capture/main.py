e277d65d7f1320395e7182ff1141d0303dacf22b
Kurosovaya <nonickk9@gmail.com>
2026-02-06 17:11:57 +0300

    ач

--- cv_eve/screen_capture/main.py
+++ cv_eve/screen_capture/main.py
@@ -1,10 +1,12 @@
-from lib.camera.bettercam import WindowsCapture
+import dxcam
 import pyglet
 from threading import Event, Lock, Thread
 import time
 from ultralytics.models import YOLO
 import queue
 import numpy as np
+from lib.get_window.get_window import GetWindow
+import cv2
 
 
 latest_vis = None
@@ -12,10 +14,11 @@ INFER_FPS = 30
 stop_event = Event()
 frame_lock = Lock()
 infer_q = queue.Queue(maxsize=1)
-capture = WindowsCapture("EVE Launc")
-window = pyglet.window.Window(width=capture.VIDEO_OUTPUT_WIDTH,
-                                height=capture.VIDEO_OUTPUT_HEIGHT,
-                                caption="Output", resizable=True)
+fucking_window = GetWindow("EVE Launcher")
+capture = dxcam.create(region=fucking_window.get_region())
+window = pyglet.window.Window(width=960,
+                                height=540,
+                                caption="Output", resizable=False)
 fps_display = pyglet.window.FPSDisplay(window)
 model = YOLO(r"D:\\Projects\\my_ml_backend\\models\\RectangleLabelsObbModel\\weights\\best.pt")
 img = None
@@ -41,10 +44,10 @@ def on_draw():
         _buf = bytearray(w * h * 3)
         _buf_np = np.frombuffer(_buf, dtype=np.uint8)
         _buf_np[:] = frame.reshape(-1)
-        img = pyglet.image.ImageData(w, h, 'BGR', _buf, pitch=-w*3)
+        img = pyglet.image.ImageData(w, h, 'RGB', _buf, pitch=-w*3)
     else:
         _buf_np[:] = frame.reshape(-1)
-        img.set_data('BGR', -w * 3, _buf) 
+        img.set_data('RGB', -w * 3, _buf) 
     img.blit(0, 0, width=window.width, height=window.height)
     fps_display.draw()
 
@@ -67,13 +70,12 @@ def screen_capture():
 
 def inference_thread_func():
     global latest_vis
+    infer_period = 1.0 / INFER_FPS
+    last = 0.0
     while True:
         frame_bgr = infer_q.get()
         if frame_bgr is None:
             continue
-
-        infer_period = 1.0 / INFER_FPS
-        last = 0.0
 
         now = time.perf_counter()
         if now - last < infer_period:
@@ -85,10 +87,24 @@ def inference_thread_func():
             save=False, stream=False, verbose=False, device="cpu"
         )
         res = results[0]
-        vis_bgr = res.plot()
+        polys = res.obb.xyxyxyxy
+        confs = res.obb.conf
+        clss  = res.obb.cls
 
+        polys = polys.cpu().numpy().astype(np.int32)   # (N,4,2)
+        confs = confs.cpu().numpy()
+        clss  = clss.cpu().numpy().astype(int)
+
+        for poly, conf, cls_id in zip(polys, confs, clss):
+            pts = poly.astype(np.int32).reshape(-1, 1, 2)  # (4,1,2)
+            cv2.polylines(frame_bgr, [pts], isClosed=True, color=(0, 255, 0), thickness=2)
+            label = f"{res.names[int(cls_id)]} {conf:.2f}"
+            x, y = pts[0, 0]
+            cv2.putText(frame_bgr, label, (x, max(0, y - 5)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+            
         with frame_lock:
-            latest_vis = vis_bgr
+            latest_vis = frame_bgr
 
 def update(dt):
     window.dispatch_event('on_draw')
@@ -102,15 +118,15 @@ def _on_close_handler():
 window.push_handlers(on_close=_on_close_handler)
 capture_thread = Thread(target=screen_capture, daemon=True)
 inference_thread = Thread(target=inference_thread_func, daemon=True)
-pyglet.clock.schedule_interval(update, 1.0 / capture.TARGET_FPS)
+pyglet.clock.schedule_interval(update, 1.0 / INFER_FPS)
 
 if __name__ == "__main__":
     try:
-        capture.start_capture()
+        capture.start(target_fps=INFER_FPS)
         capture_thread.start()
         inference_thread.start()
         pyglet.app.run()
     finally:
-        capture.stop_capture()
+        capture.stop()
         stop_event.set()
         capture_thread.join(timeout=1.0)
